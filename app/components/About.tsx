afd10824c4410ab6c61e0dadec03891d18a4e89b
Max lambert <max.lambert429@gmail.com>
2025-11-28 13:51:58 -0700

    Grammar change

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -21,7 +21,9 @@ export default function About() {
                 <div className="titleBar"></div>
               </div>
 <h2 className="aboutBody">
-Pursuing a degree in Management of Information Systems at the University of New Mexico’s Anderson School of Management, I’m passionate about software engineering, web development, cloud solutions, and solving complex problems. <br></br><br></br>As the Principal Software Engineer of SquareScan — an award-winning SaaS startup serving real estate and contracting companies — I work across the full stack building mobile applications and platform services that support our users and push the product forward, and I’m actively seeking opportunities to continue growing as a developer.
+Pursuing a degree in Management of Information Systems at the University of New Mexico’s Anderson School of Management, I’m passionate about software engineering, web development, cloud solutions, and solving complex problems.
+<br></br><br></br>
+As the Principal Software Engineer of SquareScan, an award-winning SaaS startup serving real estate and contracting companies, I work across the full stack building mobile applications and platform services that support our users and push the product forward. I’m actively seeking opportunities to continue growing as a developer.
 </h2>
     <FadeInOnScroll>
         <span style={{display:"flex", flexDirection:"column", alignItems:"center"}}>
